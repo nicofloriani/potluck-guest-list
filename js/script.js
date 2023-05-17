9d197507f1b07a539ff6a0a4ed66cf1902187a9d
nico_floriani <floriani.nico@gmail.com>
2023-05-17 14:31:55 -0700

    Added functions to limit guest list, update number of guests attending, and change classes when limit reached

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ addGuestButton.addEventListener("click", function () {
     //console.log(guest);
     if (guest !== "") {
       addToList(guest);
+      updateGuestCount();
       clearInput();
     }
   });
@@ -28,4 +29,16 @@ addGuestButton.addEventListener("click", function () {
     const listItem = document.createElement("li");
     listItem.innerText = guest;
     guestList.append(listItem);
+  };
+  
+  const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
+  
+    if (guests.length === 8) {
+      addGuestButton.classList.add("hide");
+      guestInput.classList.add("hide");
+      guestInputLabel.classList.add("hide");
+      guestFull.classList.remove("hide");
+    }
   };
