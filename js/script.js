436d4ea6b795060d0ddc5a76ad48ca32e697bda6
nico_floriani <floriani.nico@gmail.com>
2023-05-18 11:54:09 -0700

    Added functions to randomly assign a dish to guests and to disable the assign button after clicked

--- js/script.js
+++ js/script.js
@@ -10,35 +10,74 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+// assign button
+const assignButton = document.querySelector(".assign");
+// assigned items
+const assignedItems = document.querySelector(".assigned-items");
 
 addGuestButton.addEventListener("click", function () {
-    const guest = guestInput.value;
-    //console.log(guest);
-    if (guest !== "") {
-      addToList(guest);
-      updateGuestCount();
-      clearInput();
-    }
-  });
-  
-  const clearInput = function () {
-    guestInput.value = "";
-  };
-  
-  const addToList = function (guest) {
-    const listItem = document.createElement("li");
-    listItem.innerText = guest;
-    guestList.append(listItem);
-  };
-  
-  const updateGuestCount = function () {
-    const guests = document.querySelectorAll(".guest-list li");
-    guestCount.innerText = guests.length;
-  
-    if (guests.length === 8) {
-      addGuestButton.classList.add("hide");
-      guestInput.classList.add("hide");
-      guestInputLabel.classList.add("hide");
-      guestFull.classList.remove("hide");
-    }
-  };
+  const guest = guestInput.value;
+  //console.log(guest);
+  if (guest !== "") {
+    addToList(guest);
+    updateGuestCount();
+    clearInput();
+  }
+});
+
+const clearInput = function () {
+  guestInput.value = "";
+};
+
+const addToList = function (guest) {
+  const listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+  const guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
+
+const assignItems = function () {
+  const potluckItems = [
+    "cheese",
+    "salami",
+    "herbed olives",
+    "variety of crackers",
+    "fresh fruit",
+    "bruschetta",
+    "stuffed peppers",
+    "lemony chickpea salad",
+    "roasted eggplant spread",
+    "marinated zucchini",
+    "farro and cucumber salad",
+    "pesto"
+  ];
+
+  const allGuests = document.querySelectorAll(".guest-list li");
+
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
+
+    potluckItems.splice(randomPotluckIndex, 1);
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
